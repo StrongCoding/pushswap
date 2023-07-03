9c1903bd2b5d1f6773d66eb916803fae19eb51bb
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-03 15:09:35 +0200

    declaration changed

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 17:38:12 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/03 09:35:17 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/03 14:50:50 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,7 @@ void	ft_swap(t_list **stack);
 void	ft_push(t_list **stack_src, t_list **stack_dst);
 void	ft_rotate(t_list **stack);
 void	ft_reverse_rotate(t_list **stack);
+int		ft_atoi_special(char *str, int *error);
+int		*ft_new_number(int number);
 
 #endif
