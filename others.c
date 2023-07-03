c08c06142c73d678ab747f4183c6f8ef0b0de7c8
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-03 10:18:03 +0200

    added print_stacks

---
+++ others.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   others.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 15:08:29 by dnebatz           #+#    #+#             */
+/*   Updated: 2023/07/03 10:11:51 by dnebatz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_print_stack(t_list **stack)
+{
+	t_list	*tmp;
+	if (stack == NULL || *stack == NULL)
+		return ;
+	printf("stack: %i adress: %p next-> %p\n", *(int *)(*stack)->content, *stack, (*stack)->next);
+	tmp = *stack;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		printf("stack: %i adress: %p next-> %p\n", *(int *)tmp->content, tmp, tmp->next);
+	}
+}
+
+void	ft_print_stacks(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tmp;
+	t_list	*tmpb;
+
+	tmp = *stack_a;
+	tmpb = *stack_b;
+	if (tmp == NULL && tmpb == NULL)
+		return ;
+	printf("stack a:");
+	if (tmp != NULL)
+		printf(" %i", *(int *)tmp->content);
+	else
+		printf("EMPTY");
+	printf(" stack b: ");
+	if (tmpb != NULL)
+		printf(" %i\n", *(int *)tmpb->content);
+	else
+		printf("EMPTY\n");
+	//printf("stack: %i adress: %p next-> %p\n", *(int *)(*stack)->content, *stack, (*stack)->next);
+	while ((tmp != NULL || tmpb != NULL))
+	{
+		printf("stack a:");
+		if (tmp != NULL && tmp->next != NULL)
+			printf(" %i", *(int *)tmp->next->content);
+		else
+			printf(" E");
+		if (tmp != NULL)
+			tmp = tmp->next;
+		printf(" stack b: ");
+		if (tmpb != NULL && tmpb->next != NULL)
+			printf(" %i\n", *(int *)tmpb->next->content);
+		else
+			printf(" E\n");
+		if (tmpb != NULL)	
+			tmpb = tmpb->next;
+		//printf("stack_a->next: %p\n", tmp->next);
+		//printf("stack_b->next: %p\n", tmpb->next);
+		//printf("stack: %i adress: %p next-> %p\n", *(int *)tmp->content, tmp, tmp->next);
+	}
+}
