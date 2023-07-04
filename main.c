50869c0c25a5b9a456df309be393c529d4873a44
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-04 21:13:18 +0200

    added test cases

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 17:37:46 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/03 15:52:18 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/04 20:59:02 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,37 @@ int	main(int argc, char **argv)
 	t_list	*stack_a;
 	t_list	*stack_b;
 	int		error;
+	int		count;
 
 	error = 0;
+	count = 0;
 	stack_a = NULL;
 	stack_b = NULL;
-	if (ft_fill_s(&stack_a, &stack_b, argc, argv))
-		printf("error\n");
+	if (ft_fill_s(&stack_a, argc, argv, &count))
+		write(2, "Error\n", 6);
+	else
+	{
+		ft_print_stacks(&stack_a, &stack_b);
+		if (count < 3)
+			write(2, "Error\n", 6);
+		else if (count == 3)
+		{
+			printf("sort three\n");
+			ft_sort_three(&stack_a);
+		}
+		else if (count > 3 && count < 6)
+		{
+			printf("sort more than three\n");
+			ft_sort_until_five(&stack_a, &stack_b, count);
+		}
+		else
+		{
+			printf("sort a lot\n");
+			ft_sort_a_lot(&stack_a, &stack_b, count);
+		}
+	}
 	ft_print_stacks(&stack_a, &stack_b);
+	printf("count: %i\n", count);
+
+	
 }
