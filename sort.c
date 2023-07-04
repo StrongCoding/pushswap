78bec4e3a8310c9ca8193a9bbdc7689137dc0910
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-04 21:20:31 +0200

    test changes

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 11:23:16 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/04 21:09:34 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/04 21:19:33 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,12 +66,12 @@ void	ft_sort_until_five(t_list **stack_a, t_list **stack_b, int count)
 	ft_sort_three(stack_a);
 	if (push_count == 1)
 	{
-		if (**((int *)stack_a)->content == 7)
+		if (**((int **)stack_a)->content == 7)
 			printf("ist gleich\n");
 	}
 }
 
-void 	ft_sort_a_lot(t_list **stack_a, t_list **stack_b, int count)
+void	ft_sort_a_lot(t_list **stack_a, t_list **stack_b, int count)
 {
 	int	stack_count;
 
