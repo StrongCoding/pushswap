e8787c194389b4ff6bc56b58f792887f0635121f
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-04 21:13:40 +0200

    changed some stuff

--- operation_utils.c
+++ operation_utils.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 15:03:47 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/03 09:21:00 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/04 16:53:06 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,9 @@ void	ft_swap(t_list **stack)
 	if (stack == NULL || (*stack)->next == NULL)
 		return ;
 	tmp = *stack;
-	//tmp->next = (*stack)->next->next;
-	//printf("before stack adress: %p stack->next: %p stack->next-next: %p\n", *stack, (*stack)->next,(*stack)->next->next);
 	*stack = (*stack)->next;
 	tmp->next = (*stack)->next;
 	(*stack)->next = tmp;
-	//printf("after stack adress: %p stack->next: %p stack->next->next: %p\n", *stack, (*stack)->next, (*stack)->next->next);
 }
 
 void	ft_push(t_list **stack_src, t_list **stack_dst)
@@ -35,14 +32,12 @@ void	ft_push(t_list **stack_src, t_list **stack_dst)
 		return ;
 	if (*stack_dst == NULL)
 	{
-		//printf("stack_dst is leer!\n");
 		*stack_dst = *stack_src;
 		*stack_src = (*stack_src)->next;
 		(*stack_dst)->next = NULL;
 	}
 	else
 	{
-		//printf("stack_dst is nicht leer!\n");
 		tmp = *stack_dst;
 		*stack_dst = *stack_src;
 		*stack_src = (*stack_src)->next;
@@ -60,12 +55,10 @@ void	ft_rotate(t_list **stack)
 	tmp = *stack;
 	tmp2 = *stack;
 	*stack = (*stack)->next;
-	//printf("before stack adress: %p stack->next: %p stack->next-next: %p\n", *stack, (*stack)->next,(*stack)->next->next);
 	while (tmp->next != NULL)
 		tmp = tmp->next;
 	tmp->next = tmp2;
 	tmp2->next = NULL;
-	//printf("after stack adress: %p stack->next: %p stack->next->next: %p\n", *stack, (*stack)->next, (*stack)->next->next);
 }
 
 void	ft_reverse_rotate(t_list **stack)
@@ -77,13 +70,9 @@ void	ft_reverse_rotate(t_list **stack)
 		return ;
 	tmp = *stack;
 	tmp2 = *stack;
-	//printf("before stack adress: %p stack->next: %p stack->next-next: %p\n", *stack, (*stack)->next,(*stack)->next->next);
 	while (tmp->next->next != NULL)
 		tmp = tmp->next;
-	//tmp->next ist das letzte Element also muss das auf erste
 	*stack = tmp->next;
 	(*stack)->next = tmp2;
 	tmp->next = NULL;
-	//*stack = tmp->next;
-	//printf("after stack adress: %p stack->next: %p stack->next->next: %p\n", *stack, (*stack)->next, (*stack)->next->next);
 }
