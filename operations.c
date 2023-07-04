b32cc7d85e8face2acf8c895fb19faa034f43aba
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-04 21:14:26 +0200

    just test changes

--- operations.c
+++ operations.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 13:47:59 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/03 13:53:13 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/04 19:07:14 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,21 @@
 
 // sa (swap a): Swap the first 2 elements at the top of stack a.
 // Do nothing if there is only one or no elements.
+void	ft_swap(t_list **stack_a, t_list **stack_b, char* str)
+{
+	t_list	*tmp;
 
+	if (stack == NULL || (*stack)->next == NULL)
+		return ;
+	tmp = *stack;
+	*stack = (*stack)->next;
+	tmp->next = (*stack)->next;
+	(*stack)->next = tmp;
+}
 // sb (swap b): Swap the first 2 elements at the top of stack b.
 // Do nothing if there is only one or no elements.
-
+ft_push(stack_a, stack_b);
+if (*(int *)stack_a->content)
 // ss : sa and sb at the same time.
 
 // pa (push a): Take the first element at the top of b and put it at the top of a.
@@ -41,3 +52,12 @@
 // The last element becomes the first one.
 
 // rrr : rra and rrb at the same time.
+
+ft_check_for_lowest_costs()
+
+102
+7
+345
+177
+23
+1
