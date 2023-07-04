47b694e5938d5bbc995fb909639f8fbcb7a8d932
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-04 21:14:16 +0200

    declarations added

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 17:38:12 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/03 15:47:25 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/04 21:01:17 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,9 @@ void	ft_rotate(t_list **stack);
 void	ft_reverse_rotate(t_list **stack);
 int		ft_atoi_special(char *str, int *error);
 int		*ft_new_number(int number);
-int	ft_fill_s(t_list **stack_a, t_list **stack_b, int argc, char **argv);
+int		ft_fill_s(t_list **stack_a, int argc, char **argv, int *count);
+void	ft_sort_three(t_list **stack);
+void	ft_sort_until_five(t_list **stack_a, t_list **stack_b, int count);
+void 	ft_sort_a_lot(t_list **stack_a, t_list **stack_b, int count);
 
 #endif
