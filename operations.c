6f4808ae8a9fe234bece132778208cd69b1dfbc3
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-05 10:19:19 +0200

    added push and swap operations with output

--- operations.c
+++ operations.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 13:47:59 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/04 19:07:14 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/05 10:13:49 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,40 @@
 
 // sa (swap a): Swap the first 2 elements at the top of stack a.
 // Do nothing if there is only one or no elements.
-void	ft_swap(t_list **stack_a, t_list **stack_b, char* str)
+void	ft_swap_a(t_list **stack)
 {
-	t_list	*tmp;
-
-	if (stack == NULL || (*stack)->next == NULL)
-		return ;
-	tmp = *stack;
-	*stack = (*stack)->next;
-	tmp->next = (*stack)->next;
-	(*stack)->next = tmp;
+	ft_swap(stack);
+	write(1, "sa\n", 3);
 }
+
 // sb (swap b): Swap the first 2 elements at the top of stack b.
 // Do nothing if there is only one or no elements.
-ft_push(stack_a, stack_b);
-if (*(int *)stack_a->content)
+void	ft_swap_b(t_list **stack)
+{
+	ft_swap(stack);
+	write(1, "sb\n", 3);
+}
+
 // ss : sa and sb at the same time.
+void	ft_swap_s(t_list **stack_a, t_list **stack_b)
+{
+	ft_swap(stack_a);
+	ft_swap(stack_b);
+	write(1, "ss\n", 3);
+}
 
 // pa (push a): Take the first element at the top of b and put it at the top of a.
 // Do nothing if b is empty.
+void	ft_push_a(t_list **stack)
+{
+	ft_push(stack);
+	write(1, "pa\n", 3);
+}
 
 // pb (push b): Take the first element at the top of a and put it at the top of b.
 // Do nothing if a is empty.
-
-// ra (rotate a): Shift up all elements of stack a by 1.
-// The first element becomes the last one.
-
-// rb (rotate b): Shift up all elements of stack b by 1.
-// The first element becomes the last one.
-
-// rr : ra and rb at the same time.
-
-// rra (reverse rotate a): Shift down all elements of stack a by 1.
-// The last element becomes the first one.
-
-// rrb (reverse rotate b): Shift down all elements of stack b by 1.
-// The last element becomes the first one.
-
-// rrr : rra and rrb at the same time.
-
-ft_check_for_lowest_costs()
-
-102
-7
-345
-177
-23
-1
+void	ft_push_b(t_list **stack)
+{
+	ft_push(stack);
+	write(1, "pb\n", 3);
+}
