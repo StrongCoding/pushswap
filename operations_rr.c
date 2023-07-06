fdd642a6740385304856659639526c920b1a84a0
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-06 08:58:44 +0200

    header changed

--- operations_rr.c
+++ operations_rr.c
@@ -1,3 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations_rr.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 10:35:36 by dnebatz           #+#    #+#             */
+/*   Updated: 2023/07/05 10:35:58 by dnebatz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
 // rra (reverse rotate a): Shift down all elements of stack a by 1.
 // The last element becomes the first one.
 void	ft_reverse_rotate_a(t_list **stack)
