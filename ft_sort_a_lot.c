3c2ee0e0fa84b7aad571eb986154e118ffad0910
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-10 21:20:29 +0200

    added new algorithm

--- ft_sort_a_lot.c
+++ ft_sort_a_lot.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 09:50:34 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/10 14:52:03 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/10 19:14:53 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,10 +46,10 @@ int	ft_get_shortest_way_rev(t_list **stack, int number)
 		i++;
 		if (!(tmp->next == NULL))
 		{
-			if (DEBUG)
-				printf("content: %i <= number: %i next: %i >= number: %i\n", *(int *)tmp->content, number, *(int *)tmp->next->content, number);
-			if (DEBUG)
-				printf("aktuelle %i < nächste %i UND nächste %i < number %i\n", *(int *)tmp->content, *(int *)tmp->next->content, *(int *)tmp->next->content, number);
+			// if (DEBUG)
+			// 	printf("content: %i <= number: %i next: %i >= number: %i\n", *(int *)tmp->content, number, *(int *)tmp->next->content, number);
+			// if (DEBUG)
+			// 	printf("aktuelle %i < nächste %i UND nächste %i < number %i\n", *(int *)tmp->content, *(int *)tmp->next->content, *(int *)tmp->next->content, number);
 		}	
 		else
 			if (DEBUG)
@@ -88,16 +88,10 @@ void	ft_push_sort(t_list **stack_a, t_list **stack_b, int stack_c_items)
 				ft_reverse_rotate_b(stack_b);
 		ft_push_b(stack_a, stack_b);
 	}
-	way = ft_get_shortest_way_rev(stack_b, 2147483647);
-	if (way > 0)
-		while (way-- > 0)
-			ft_rotate_b(stack_b);
-	else if (way < 0)
-		while (way++ < 0)
-			ft_reverse_rotate_b(stack_b);
-	i = 0;
-	while (i++ < stack_c_items)
-		ft_push_a(stack_b, stack_a);
+	//ft_get_highest_top_rev(stack_b);
+	// i = 0;
+	// while (i++ < stack_c_items)
+	// 	ft_push_a(stack_b, stack_a);
 }
 
 void	ft_sort_a_lot(t_list **stack_a, t_list **stack_b, int count)
@@ -107,7 +101,56 @@ void	ft_sort_a_lot(t_list **stack_a, t_list **stack_b, int count)
 	int	i;
 
 	i = 0;
-	stack_count = 1;
+	stack_count = 2;
 	stack_c_items = count / stack_count;
 	ft_push_sort(stack_a, stack_b, stack_c_items);
+	ft_get_highest_top_rev(stack_b);
+	ft_sort_until_five(stack_a, stack_b, stack_c_items);
+	ft_merge_sorted_stacks(stack_a, stack_b, stack_c_items);
+	ft_get_highest_top(stack_a);
+}
+
+void	ft_get_highest_top_rev(t_list **stack)
+{
+	int	way;
+
+	way = ft_get_shortest_way_rev(stack, 2147483647);
+	if (way > 0)
+		while (way-- > 0)
+			ft_rotate_b(stack);
+	else if (way < 0)
+		while (way++ < 0)
+			ft_reverse_rotate_b(stack);
+}
+
+void	ft_get_highest_top(t_list **stack)
+{
+	int	way;
+
+	way = ft_get_shortest_way(stack, 2147483647);
+	if (way > 0)
+		while (way-- > 0)
+			ft_rotate_a(stack);
+	else if (way < 0)
+		while (way++ < 0)
+			ft_reverse_rotate_a(stack);
+}
+
+void	ft_merge_sorted_stacks(t_list **stack_a, t_list **stack_b, int count)
+{
+	int	i;
+	int	way;
+
+	i = 0;
+	while (i++ < count)
+	{
+		way = ft_get_shortest_way(stack_a, *(int *)(*stack_b)->content);
+		if (way > 0)
+			while (way-- > 0)
+				ft_rotate_a(stack_a);
+		else if (way < 0)
+			while (way++ < 0)
+				ft_reverse_rotate_a(stack_a);
+		ft_push_a(stack_b, stack_a);
+	}
 }
