147ebc333505d7ab8c99710b98d3313b2175b0e2
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-12 17:06:06 +0200

    fixed find way potions and added merge loop

--- ft_sort_five_hundred.c
+++ ft_sort_five_hundred.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 09:58:02 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/11 12:25:11 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/12 16:50:41 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ void	ft_sort_five_hundred(t_list **stack_a, t_list **stack_b, int count)
 	i = 0;
 	j = 0;
 	potion_size = 50;
-	stack_count = 2;
+	stack_count = 10;
 	while (j++ < stack_count - 1)
 	{
-		ft_push_sort_not_reverse(stack_a, stack_b, potion_size);
+		ft_push_sort(stack_a, stack_b, potion_size);
 		ft_get_highest_top_rev(stack_b);
 		i = 0;
 		while (i++ < potion_size)
@@ -34,43 +34,68 @@ void	ft_sort_five_hundred(t_list **stack_a, t_list **stack_b, int count)
 			ft_rotate_a(stack_a);
 		}
 	}
-	ft_push_sort_not_reverse(stack_a, stack_b, potion_size);
+	i = 0;
+	ft_push_sort(stack_a, stack_b, potion_size);
 	ft_get_highest_top_rev(stack_b);
-	//ft_merge_sorted_potions(stack_a, stack_b, potion_size);
-	ft_print_stacks(stack_a, stack_b);
+	while (i++ < stack_count - 1)
+		ft_merge_sorted_potions(stack_a, stack_b, potion_size);
+	i = 0;
+	while (i++ < count)
+		ft_push_a(stack_b, stack_a);
+	//ft_print_stacks(stack_a, stack_b);
 }
 
 void	ft_merge_sorted_potions(t_list **stack_a, t_list **stack_b, int potion_count)
 {
 	int	i;
 	int	way;
-	int	j;
 
 	i = 0;
-	j = 0;
 	while (i++ < potion_count)
 	{
-		way = ft_get_shortest_potion_way(stack_a, *(int *)(*stack_b)->content, potion_count);
-		while (way-- > 0)
+		way = ft_get_shortest_potion_way(stack_b, *(int *)(*stack_a)->content, potion_count);
+		//printf("way: %i\n", way);
+		while (way > 0)
 		{
-			ft_rotate_a(stack_a);
-			j++;
+			ft_rotate_b(stack_b);
+			way--;
 		}
-		ft_push_a(stack_b, stack_a);
+		ft_push_b(stack_a, stack_b);
+		if (way == -1)
+		{
+			ft_reverse_rotate_b(stack_b);
+		}
 	}
-	while (j++ < potion_count)
-		ft_rotate_a(stack_a);
+	ft_get_highest_top_rev(stack_b);
 }
 
 int	ft_get_shortest_potion_way(t_list **stack, int number, int potion_count)
 {
 	t_list	*tmp;
 	int		i;
+	int		lowest;
 
+	tmp = *stack;
+	lowest = *(int *)tmp->content;
+	while (tmp->next != NULL)
+	{
+		if (*(int *)tmp->next->content < lowest)
+			lowest = *(int *)tmp->next->content;
+		tmp = tmp->next;
+	}
 	i = 0;
 	tmp = *stack;
-	while (i++ > potion_count)
+	// wenn kleinste zahl, dann muss die gepuscht werden und way doch negativ also reverserotaten 1x
+	//printf("lowest: %i > number %i && content %i == lowest %i\n", lowest, number, *(int *)tmp->content, lowest);
+	// if (lowest > number)
+	// 	return (-1);
+	while (tmp->next != NULL)
+	//while (i < potion_count)
 	{
+		//printf("content %i < number%i\n", *(int *)tmp->content, number);
+		//if ((*(int *)tmp->content < number) || *(int *)tmp->content == lowest)
+		if (*(int *)tmp->content == lowest && number < lowest)
+				return (i + 1);
 		if ((*(int *)tmp->content < number))
 		{
 			if (DEBUG)
@@ -78,6 +103,7 @@ int	ft_get_shortest_potion_way(t_list **stack, int number, int potion_count)
 			return (i);
 		}
 		tmp = tmp->next;
+		i++;
 	}
 	return (0);
 }
@@ -103,8 +129,4 @@ void	ft_push_sort_not_reverse(t_list **stack_a, t_list **stack_b, int stack_c_it
 				ft_rotate_b(stack_b);
 		ft_push_b(stack_a, stack_b);
 	}
-	//ft_get_highest_top_rev(stack_b);
-	// i = 0;
-	// while (i++ < stack_c_items)
-	// 	ft_push_a(stack_b, stack_a);
 }
