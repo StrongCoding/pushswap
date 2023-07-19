bca29c926d6b8f415cc7451846b2805e4b4ace72
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-19 19:22:33 +0200

    changed error handling to the one from old special

--- ft_atoi_special.c
+++ ft_atoi_special.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 14:35:25 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/19 11:21:55 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/19 19:21:07 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@ int	ft_set_error(int *error)
 {
 	*error = 1;
 	return (0);
+}
+
+int	ft_check_after(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] >= '0' && str [i] <= '9')
+		i++;
+	if (str[i])
+		return (1);
+	else
+		return (0);
 }
 
 int	ft_atoi_s(char *str, int *error)
@@ -29,8 +42,8 @@ int	ft_atoi_s(char *str, int *error)
 	number = 0;
 	if (!str[0])
 		return (ft_set_error(error));
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
+	if (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		return (ft_set_error(error));
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i++] == '-')
@@ -39,6 +52,8 @@ int	ft_atoi_s(char *str, int *error)
 	while (str[i] && str[i] >= '0' && str[i] <= '9')
 		number = (number * 10) + str[i++] - '0';
 	number *= count_minus;
+	if (ft_check_after(&str[i]))
+		return (ft_set_error(error));
 	if (i && number <= 2147483647 && number >= -2147483648)
 		return (number);
 	else
