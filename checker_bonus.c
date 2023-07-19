2852cae7517d0cf4dfeb02160c84250a6636498e
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-19 19:44:55 +0200

    added & to lstclear

--- checker_bonus.c
+++ checker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 18:44:42 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/19 19:39:28 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/19 19:43:46 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,6 +107,6 @@ int	main(int argc, char **argv)
 	else
 		ft_get_inputs(&error, &stack_a, &stack_b);
 	ft_check_sorted_print(&error, &stack_a, &stack_b);
-	ft_lstclear(stack_a, free);
-	ft_lstclear(stack_b, free);
+	ft_lstclear(&stack_a, free);
+	ft_lstclear(&stack_b, free);
 }
