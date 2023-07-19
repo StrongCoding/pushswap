1476b24eda2a73d2ee7329c8d4a8e8d52ad430bd
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-19 17:54:04 +0200

    no changes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 17:37:46 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/19 10:37:18 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/19 11:23:26 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,10 +59,6 @@ int	main(int argc, char **argv)
 	ft_lstclear(&stack_a, free);
 }
 
-// cc main.c libftprintf.a operation_utils.c others.c
-// ft_atoi_special.c sort_less.c ft_sort_a_lot.c operations_rotate.c
-// operations_rr.c operations_ps.c  ft_sort_two.c ft_sort_five_hundred.c
-// sort_a_lot_utils.c dontuploadthis.c errors.c -o push_swap
 // "; ./a.out $ARG | ./checker_Mac $ARG
 // ARG="515 -9805 52 1476 -4131"; ./a.out $ARG | wc -l
 // valgrind --leak-check=full -s ./a.out 3 1 2
