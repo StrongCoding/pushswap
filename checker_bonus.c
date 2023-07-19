8a4f1794c05a9b4e976b44a7213c9999665f185c
Daniel Nebatz <dnebatz@student.42wolfsburg.de>
2023-07-19 17:53:29 +0200

    added check for sort and double commands

--- checker_bonus.c
+++ checker_bonus.c
@@ -6,41 +6,86 @@
 /*   By: dnebatz <dnebatz@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 18:44:42 by dnebatz           #+#    #+#             */
-/*   Updated: 2023/07/19 09:50:45 by dnebatz          ###   ########.fr       */
+/*   Updated: 2023/07/19 17:53:17 by dnebatz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_check_statement(char *s)
+void	ft_check_sorted_print(t_list **stack_a, t_list **stack_b)
+{
+	if (ft_lstsize(*stack_b))
+		write(2, "KO\n", 4);
+	else if (ft_check_sorted(stack_a))
+		write(2, "OK\n", 3);
+	else
+		write(2, "KO\n", 3);
+}
+
+void	ft_double_commands(char *s, t_list **stack_a, t_list **stack_b, int len)
+{
+	if (!ft_strncmp("ss\n", s, len))
+	{
+		ft_swap(stack_a);
+		ft_swap(stack_b);
+	}
+	else if (!ft_strncmp("rr\n", s, len))
+	{
+		ft_rotate(stack_a);
+		ft_rotate(stack_b);
+	}
+	else if (!ft_strncmp("rrr\n", s, len))
+	{
+		ft_reverse_rotate(stack_a);
+		ft_reverse_rotate(stack_b);
+	}
+}
+
+int	ft_check_statement(char *s, int *error, t_list **stack_a, t_list **stack_b)
 {
 	int	len;
 
 	len = ft_strlen(s);
 	if (!ft_strncmp("sa\n", s, len))
-		ft_printf("ist sa\n");
+		ft_swap(stack_a);
 	else if (!ft_strncmp("sb\n", s, len))
-		ft_printf("ist sb\n");
+		ft_swap(stack_b);
 	else if (!ft_strncmp("pa\n", s, len))
-		ft_printf("ist pa\n");
+		ft_push(stack_b, stack_a);
 	else if (!ft_strncmp("pb\n", s, len))
-		ft_printf("ist pa\n");
+		ft_push(stack_a, stack_b);
 	else if (!ft_strncmp("ra\n", s, len))
-		ft_printf("ist ra\n");
+		ft_rotate(stack_a);
 	else if (!ft_strncmp("rb\n", s, len))
-		ft_printf("ist rb\n");
+		ft_rotate(stack_b);
 	else if (!ft_strncmp("rra\n", s, len))
-		ft_printf("ist rra\n");
+		ft_reverse_rotate(stack_a);
 	else if (!ft_strncmp("rrb\n", s, len))
-		ft_printf("ist rrb\n");
+		ft_reverse_rotate(stack_b);
+	else if (!ft_strncmp("rr\n", s, len) || !ft_strncmp("rrr\n", s, len) || 
+		!ft_strncmp("ss\n", s, len))
+		ft_double_commands(s, stack_a, stack_b, len);
 	else
-		write(2, "Error\n", 6);
+		*error = 1;
 	return (0);
+}
+
+void	ft_get_inputs(int *count, t_list **stack_a, t_list **stack_b)
+{
+	char	*input;
+
+	input = NULL;
+	input = get_next_line(0);
+	while (input && *input != '\n')
+	{
+		ft_check_statement(input, count, stack_a, stack_b);
+		input = get_next_line(0);
+	}
+	free(input);
 }
 
 int	main(int argc, char **argv)
 {
-	char	*input;
 	t_list	*stack_a;
 	t_list	*stack_b;
 	int		count;
@@ -59,17 +104,11 @@ int	main(int argc, char **argv)
 	else if (!ft_check_double(&stack_a))
 		write(2, "Error\n", 6);
 	else
-	{
-		input = get_next_line(0);
-		while (input && *input != '\n')
-		{
-			ft_check_statement(input);
-			input = get_next_line(0);
-		}
-		free(input);
-	}
+		ft_get_inputs(&count, &stack_a, &stack_b);
+	if (count < 2)
+		write(2, "Error\n", 6);
+	else
+		ft_check_sorted_print(&stack_a, &stack_b);
 	ft_lstclear(&stack_a, free);
 	ft_lstclear(&stack_b, free);
 }
-
-// cc checker_bonus.c libftprintf.a errors.c others.c ft_atoi_special.c 
